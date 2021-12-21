b0a4389e5e059204bb753949b9999a2f82e100d7
RNtaate <royhyde14@gmail.com>
2021-12-21 19:56:20 +0300

    Add errors, disabled states
    Implement displaying of request, response errors using Alert component

--- src/containers/Auth/Signup.js
+++ src/containers/Auth/Signup.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { Card, Form, Button, Row, Col } from 'react-bootstrap';
+import { Card, Form, Button, Row, Col, Alert } from 'react-bootstrap';
 import RightsComponent from './RightsComponent';
 
 const Signup = () => {
@@ -15,33 +15,55 @@ const Signup = () => {
     email: '',
     password: '',
     password_confirmation: '',
-    // contribution: false,
-    // membershipFee: false,
-    // rules:false
+    contribution: false,
+    membership_fee: false,
+    rules: false
   })
+
+  const [errors, setErrors] = useState([])
+
+  const [disabled, setDisabled] = useState(false)
 
   const handleChange = (e) => {
     console.log(e.target.value)
-    setSignupInfo({ ...signupInfo, [e.target.name]: e.target.value })    
+    setSignupInfo({ ...signupInfo, [e.target.name]: e.target.value })
   }
 
   const handleCheck = (e) => {
     console.log(e.target.checked)
-    setSignupInfo({ ...signupInfo, [e.target.value]: e.target.checked})
+    setSignupInfo({ ...signupInfo, [e.target.value]: e.target.checked })
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setErrors([]);
+    setDisabled(true);
     const user = { ...signupInfo }
-    axios.post('http://localhost:3001/registrations', {user}, { withCredentials: true })
+    axios.post('http://localhost:3001/registrations', { user }, { withCredentials: true })
       .then((response) => {
-      console.log(response)
+        console.log(response)
+        setErrors([])
       }).catch((err) => {
-      console.log('Something went wrong', err)
-    })
+        if(err.response.status == 422){
+          console.log('Something went wrong', err.response.data.errors)
+          let errorsHolder = Object.entries(err.response.data.errors);
+
+          let newArray = []
+          errorsHolder.map((singleError) => newArray.push(`${singleError[0]} ${singleError[1][0]}`))
+
+          setErrors(newArray);
+        }
+        else
+        {
+          console.log('Your network might be down, please try again later');
+          setErrors(['Your network might be down, please try again later'])
+        }
+      }).finally(() => {
+        setDisabled(false);
+      })
   }
 
-  
+
 
   let selectCountries = ['Uganda', 'Kenya', 'Tanzania', 'Germany', 'Burundi'];
   selectCountries.sort();
@@ -51,11 +73,11 @@ const Signup = () => {
         <Card.Body>
           <h2 className="text-secondary text-center mb-3">Sign Up</h2>
 
-          <Form onSubmit={ handleSubmit }>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3">
               <Row>
                 <Col>
-                  <Form.Control required placeholder="First name" name='first_name' onChange= { handleChange } />
+                  <Form.Control required placeholder="First name" name='first_name' onChange={handleChange} />
                 </Col>
                 <Col>
                   <Form.Control required placeholder="Last name" name='last_name' onChange={handleChange} />
@@ -65,18 +87,18 @@ const Signup = () => {
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label> example: +1234567890</Form.Label>
-              <Form.Control required type="tel" pattern="^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$" placeholder="Telephone Number" name='telephone_number' onChange={handleChange}/>
+              <Form.Control required type="tel" pattern="^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$" placeholder="Telephone Number" name='telephone_number' onChange={handleChange} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Control required type="text" placeholder="Account Number" name='account_number' onChange={handleChange}/>
+              <Form.Control required type="text" placeholder="Account Number" name='account_number' onChange={handleChange} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Control required type="text" placeholder="Name of Bank" name='bank_name' onChange={handleChange}/>
+              <Form.Control required type="text" placeholder="Name of Bank" name='bank_name' onChange={handleChange} />
             </Form.Group>
 
-            <Form.Select className="mb-3 text-secondary" name='country' onChange= { handleChange } >
+            <Form.Select className="mb-3 text-secondary" name='country' onChange={handleChange} >
               <option>Select Country</option>
               {
                 selectCountries.map((country, index) => <option key={index}>{country}</option>)
@@ -84,7 +106,7 @@ const Signup = () => {
             </Form.Select>
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Control required type="email" placeholder="Enter email" name='email' onChange={handleChange}/>
+              <Form.Control required type="email" placeholder="Enter email" name='email' onChange={handleChange} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -92,37 +114,38 @@ const Signup = () => {
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Control required type="password" placeholder="Password Confirmation" name='password_confirmation' onChange={handleChange}/>
+              <Form.Control required type="password" placeholder="Password Confirmation" name='password_confirmation' onChange={handleChange} />
             </Form.Group>
 
             <div className="form-check mb-3">
-              <input className="form-check-input" type="checkbox" value="contribution" id="flexCheckDefault" onChange= {handleCheck} />
+              <input className="form-check-input" type="checkbox" value="contribution" id="flexCheckDefault" onChange={handleCheck} />
               <label className="form-check-label text-secondary" htmlFor="flexCheckDefault">
                 Do I wholeheartedly agree to contribute to TOGETHER WE CAN within (number of days)
               </label>
             </div>
 
             <div className="form-check mb-3">
-              <input className="form-check-input" type="checkbox" value="membershipFee" id="membershipFeeCheck" onChange={handleCheck}/>
+              <input className="form-check-input" type="checkbox" value="membership_fee" id="membershipFeeCheck" onChange={handleCheck} />
               <label className="form-check-label text-secondary" htmlFor="membershipFeeCheck">
                 Am I informed that the membership fee (the contribution paid) announced by TOGETHER WE CAN is not refundable?
               </label>
             </div>
 
             <div className="form-check mb-3">
-              <input className="form-check-input" type="checkbox" value="rules" id="rulesCheck" onChange={handleCheck}/>
+              <input className="form-check-input" type="checkbox" value="rules" id="rulesCheck" onChange={handleCheck} />
               <label className="form-check-label text-secondary" htmlFor="rulesCheck">
                 Do I comply with the rules set by TOGETHER WE CAN?
               </label>
             </div>
 
-            <Button variant="primary" type="submit" className="w-100 mt-3">
+            <Button disabled={disabled} variant="primary" type="submit" className="w-100 mt-3">
               Submit
             </Button>
           </Form>
-          <div >
-              {JSON.stringify(signupInfo)}
-          </div>
+
+          {errors.length > 0 ? <Alert variant='danger' className="mt-3">
+            { errors.map((error, index) => <p key={index}>{error}</p>)}
+          </Alert> : <p>Hold on!</p>}
         </Card.Body>
       </Card>
 
