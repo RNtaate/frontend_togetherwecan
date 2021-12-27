5bd518b39fe711c6bcef8e42f05e64dea9c736d6
RNtaate <royhyde14@gmail.com>
2021-12-28 02:19:29 +0300

    Connect Dashboard component to redux store
    Create handleLogout function

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
-const Dashboard = () => {
+import { setLoggedInStatus } from '../redux/actions';
+
+const Dashboard = (props) => {
+
+  let {userObj, setUserLoggedInStatus} = props;
+  let history = useHistory();
+
+  let handleLogout = () => {
+    axios.delete('http://localhost:3001/logout', {withCredentials: true})
+    .then((response) => {
+      console.log("Successfully looged out yay!");
+      console.log(response);
+      setUserLoggedInStatus(response.data.logged_in)
+      history.push("/login")
+    }).catch((error) => {
+      console.log('Someting went wrong', error);
+    })
+  }
+
   return (
     <div>
       <h2 className="text-secondary">Dashboard</h2>
+      <h4>Welcome {JSON.stringify(userObj.loggedin)}</h4>
+
+      <button type="button" onClick={handleLogout}>
+        Log Out
+      </button>
     </div>
   );
 };
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return {
+    userObj: state.userReducer
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setUserLoggedInStatus: (loggedInStatus) => {
+      dispatch(setLoggedInStatus(loggedInStatus));
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
