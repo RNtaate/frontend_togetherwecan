526a4cbb7b613ee9f2b20eaf228658d9ebd070cf
RNtaate <royhyde14@gmail.com>
2022-03-02 14:14:09 +0300

    Create Navbar component
    Add bootstrap Navbar to it

---
+++ src/containers/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Navbar, Container, Nav, Form, FormControl, Button, NavDropdown } from 'react-bootstrap'
+
+let NavbarComponent = ({currentUser, handleLogout}) => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">ISHAMI</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+
+            <Nav.Link href="/">Dashboard</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="#action2">About</Nav.Link>
+            <NavDropdown title={currentUser.user.first_name} id="navbarScrollingDropdown" className="dropdown-menu-right">
+              <NavDropdown.Item href="#action3" onClick={() => handleLogout()}>Logout</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                {currentUser.user.email}
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavbarComponent;
